d4610141b67f91ed11739389c88197f912405886
Aidan Smith <aidansmith1243@gmail.com>
2021-11-09 10:51:06 -0600

    Made simple login page functionality for demo

--- CardGame/CardGame/client-app/src/Pages/Login/Login.jsx
+++ CardGame/CardGame/client-app/src/Pages/Login/Login.jsx
@@ -1,9 +1,22 @@
 import TopBar from "../../Components/TopBar/Topbar";
+import { Link } from "react-router-dom";
+import './Login.css';
 
 const Login = () => {
+
+    const handleSubmit = (event) => {
+
+    }
+
     return ( 
-    <div>
+    <div className='LoginPage'>
         <TopBar/>
+        <form onSubmit={handleSubmit} action='/home'>
+            <a>Login</a>
+            <input type='text' placeholder='Username'></input>
+            <input type='password' placeholder='Password'></input>
+            <input type='submit'></input>
+        </form>
     </div> 
     );
 }
