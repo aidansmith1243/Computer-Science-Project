65a2caeda2281880c2d38d02b4a6dd2ecd8613a2
Aidan Smith <aidansmith1243@gmail.com>
2021-11-23 15:07:39 -0600

    Added prettier formatting to change indentations

--- CardGame/CardGame/client-app/src/Components/Card/Hand/Hand.jsx
+++ CardGame/CardGame/client-app/src/Components/Card/Hand/Hand.jsx
@@ -1,134 +1,97 @@
-import { useEffect, useState } from "react";
-import {Draggable,Droppable,DragDropContext} from 'react-beautiful-dnd';
-import Card from "../Card";
+import { useEffect, useState } from 'react';
+import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
+import Card from '../Card';
 
 const Hand = (props) => {
-    //const [cards,setCards] = useState([...props.cards])
-    const {x,y,rotated,release,id,cards,setCards} = props
-    //rotated = 'left','right','down'
-    const cardHeight = 140;
-    const cardWidth = 90;
-    
+  //const [cards,setCards] = useState([...props.cards])
+  const { x, y, rotated, release, id, cards, setCards } = props;
+  //rotated = 'left','right','down'
+  const cardHeight = 140;
+  const cardWidth = 90;
 
-    const hand_style = {}
-    if(rotated){
-        switch (rotated) {
-            case 'left':
-                hand_style.transform = "rotate(-90deg)"
-                break;
-            case 'right':
-                hand_style.transform = "rotate(90deg)"
-                break;
-            case 'down':
-                hand_style.transform = "rotate(-180deg)"
-                break;
-            default:
-        }
+  const hand_style = {};
+  if (rotated) {
+    switch (rotated) {
+      case 'left':
+        hand_style.transform = 'rotate(-90deg)';
+        break;
+      case 'right':
+        hand_style.transform = 'rotate(90deg)';
+        break;
+      case 'down':
+        hand_style.transform = 'rotate(-180deg)';
+        break;
+      default:
     }
+  }
 
-    const dif = 40;
-    let mod = 0-dif;
+  const dif = 40;
+  let mod = 0 - dif;
 
-    const getItems = () =>
-        cards.map(k => ({
+  const getItems = () =>
+    cards.map((k) => ({
+      id: k.suit ? `card-${k.suit} ${k.rank}` : `card-${cards.indexOf(k)}`,
+      content: <Card position={{ x: 0, y: 0 }} suit={k.suit} rank={k.rank} />,
+    }));
+  const [items, setItems] = useState(getItems());
+  useEffect(() => {
+    setItems(getItems());
+  }, [cards]);
 
-            id: k.suit ? `card-${k.suit} ${k.rank}` : `card-${cards.indexOf(k)}`,
-            content: <Card
-                position={{x:0,y:0}}
-                suit={k.suit}
-                rank={k.rank}/>}
-        ));
-    const [items,setItems] = useState(getItems());
-            useEffect(() => {
-                setItems(getItems())
-            },[cards])
-    const getItemStyle = (isDragging, draggableStyle) => ({
-        // some basic styles to make the items look a bit nicer
-        userSelect: 'none',
-        padding: grid * 2,
-        //margin: `0 ${grid}px 0 0`,
-      
-        // change background colour if dragging
-        //background: isDragging ? 'lightgreen' : 'grey',
-      
-        // styles we need to apply on draggables
-        ...draggableStyle,
-      });
-      
-      const getListStyle = isDraggingOver => ({
-        //background: isDraggingOver ? 'lightblue' : 'lightgrey',
-        display: 'flex',
-        padding: grid,
-        height: cardHeight,
-        //overflow: 'none',
-      });
-      const grid = 8;
+  const grid = 8;
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    padding: grid * 2,
+    ...draggableStyle,
+  });
 
-    return (
-        <div 
-            className="Hand"
-            style={{
-                top: y + 'px',
-                left: x + 'px',
-                width: (cardWidth + dif * cards.length) + 'px',
-                height: cardHeight + 'px',
-                position:'absolute'}}>
-            {/* <DragDropContext onDragEnd={onDragEnd}> */}
-        <Droppable droppableId={id} direction="horizontal">
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-              {...provided.droppableProps}
-            >
-              {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+  const getListStyle = (isDraggingOver) => ({
+    display: 'flex',
+    padding: grid,
+    height: cardHeight,
+  });
 
-        </div>
-    );
-    return ( <div >
-        {
-            
-            cards.map(card => {
-                console.log(mod)
-                return (
-                    <div key={card.suit + card.rank}>
-                        <Draggable 
-                            onStop={(e,data) =>release(e,data)}
-                            title="temp">
-                                
-                            <div>
-                                <Card style={hand_style}
-                                    position={{x:(mod+=dif),y:0}}
-                                    suit={card.suit}
-                                    rank={card.rank}
-                                    />
-                            </div>
-                        </Draggable>
-                    </div>
+  return (
+    <div
+      className='Hand'
+      style={{
+        top: y + 'px',
+        left: x + 'px',
+        width: cardWidth + dif * cards.length + 'px',
+        height: cardHeight + 'px',
+        position: 'absolute',
+      }}
+    >
+      <Droppable droppableId={id} direction='horizontal'>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+            {...provided.droppableProps}
+          >
+            {items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+                  >
+                    {item.content}
+                  </div>
                 )}
-            )
-        }
-    </div> );
-}
- 
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
 export default Hand;
--- CardGame/CardGame/client-app/src/Pages/Games/Hearts/Hearts.jsx
+++ CardGame/CardGame/client-app/src/Pages/Games/Hearts/Hearts.jsx
@@ -1,163 +1,159 @@
 import Card from '../../../Components/Card/Card';
 import TopBar from '../../../Components/TopBar/Topbar';
 import './Hearts.css';
-import {HubConnectionBuilder} from '@microsoft/signalr';
-import {Draggable,Droppable,DragDropContext} from 'react-beautiful-dnd';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
 import { useEffect, useState } from 'react';
 import Hand from '../../../Components/Card/Hand/Hand';
-import {setState} from 'react';
+import { setState } from 'react';
 
 const Hearts = () => {
-    const [gameConnection, setGameConnection] = useState(null)
-    
-    useEffect(() => {
-        const newConnection = new HubConnectionBuilder()
-            .withUrl('http://localhost:60230/Game')
-            .withAutomaticReconnect()
-            .build();
-        //newConnection.start();
-        //setGameConnection(newConnection)
-    },[])
-    useEffect(() => {
-        (async () => {
-            if(gameConnection){
-                gameConnection.on("GameUpdate", (move,gameState) => {
-                    console.log("GameUpdate",(move,gameState));
-                })
-                gameConnection.on("InvalidMove", (gameState) => {
-                    console.log("InvalidMove", gameState);
-                })
-                gameConnection.on("GameState", (gameState) => {
-                    console.log("GameState", gameState);
-                })
-            }
-        })();
-    },[gameConnection]);
-    const [myHand,setMyHand] = useState([
-        {suit: 'S', rank:'8'},
-        {suit: 'H', rank:'10'},
-        {suit: 'C', rank:'A'},
-        {suit: 'D', rank:'Q'},
-        {suit: 'S', rank:'10'},
-        {suit: 'H', rank:'9'},
-        {suit: 'C', rank:'Q'},
-        {suit: 'D', rank:'A'},
-    ]);
-    const [op1Hand,setOp1Hand] = useState([
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-    ]);
-    const op2_hand = [
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-    ]
-    const op3_hand = [
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-        {suit: undefined, rank:undefined},
-    ]
+  const [gameConnection, setGameConnection] = useState(null);
 
-    const getItems = (count) =>
-        Array.from({ length: count }, (v, k) => k).map(k => ({
-            id: `item-${k}`,
-            content: `item ${k}`,
-        }));
-    const [items,setItems] = useState(getItems(6));
-
-    const onDragEnd = (result) => {
-        const {source,destination} = result
-
-        // do nothing if not placed at a valid location
-        if (!destination) {
-            return;
-          }
-        
-        const sourceList = getCardList(source.droppableId);
-        const destinationList = getCardList(destination.droppableId);
-
-        // moved in its own hand
-        if(source.droppableId === destination.droppableId)
-        {
-            const newItems = reorder(
-                sourceList,
-                result.source.index,
-                result.destination.index
-              );
-            setCardList(source.droppableId,newItems)
-            return;
-        }
-        
-        // remove from current location
-        const [removedItem] = sourceList.splice(source.index,1);
-        setCardList(source.droppableId,sourceList)
-
-        // add to next location
-        destinationList.push(removedItem)
-        console.log(removedItem)
-        setCardList(destination.droppableId,destinationList)
-
-      };
-      const getCardList = (id) => {
-        switch(id)
-        {
-            case 'myHand':
-                return myHand;
-            case 'op1Hand':
-                return op1Hand;
-            default:
-                return;
-        }
+  useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+      .withUrl('http://localhost:60230/Game')
+      .withAutomaticReconnect()
+      .build();
+    //newConnection.start();
+    //setGameConnection(newConnection)
+  }, []);
+  useEffect(() => {
+    (async () => {
+      if (gameConnection) {
+        gameConnection.on('GameUpdate', (move, gameState) => {
+          console.log('GameUpdate', (move, gameState));
+        });
+        gameConnection.on('InvalidMove', (gameState) => {
+          console.log('InvalidMove', gameState);
+        });
+        gameConnection.on('GameState', (gameState) => {
+          console.log('GameState', gameState);
+        });
       }
-      const setCardList = (id,newList) => {
-        switch(id)
-        {
-            case 'myHand':
-                return setMyHand([...newList]);
-            case 'op1Hand':
-                return setOp1Hand([...newList]);
-            default:
-                return;
-        }
-      }
-      const reorder = (list, startIndex, endIndex) => {
-        const result = Array.from(list);
-        const [removed] = result.splice(startIndex, 1);
-        result.splice(endIndex, 0, removed);
-      
-        return result;
-      };
-      
-    return ( 
+    })();
+  }, [gameConnection]);
+  const [myHand, setMyHand] = useState([
+    { suit: 'S', rank: '8' },
+    { suit: 'H', rank: '10' },
+    { suit: 'C', rank: 'A' },
+    { suit: 'D', rank: 'Q' },
+    { suit: 'S', rank: '10' },
+    { suit: 'H', rank: '9' },
+    { suit: 'C', rank: 'Q' },
+    { suit: 'D', rank: 'A' },
+  ]);
+  const [op1Hand, setOp1Hand] = useState([
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+  ]);
+  const op2_hand = [
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+  ];
+  const op3_hand = [
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+    { suit: undefined, rank: undefined },
+  ];
+
+  const getItems = (count) =>
+    Array.from({ length: count }, (v, k) => k).map((k) => ({
+      id: `item-${k}`,
+      content: `item ${k}`,
+    }));
+  const [items, setItems] = useState(getItems(6));
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    // do nothing if not placed at a valid location
+    if (!destination) {
+      return;
+    }
+
+    const sourceList = getCardList(source.droppableId);
+    const destinationList = getCardList(destination.droppableId);
+
+    // moved in its own hand
+    if (source.droppableId === destination.droppableId) {
+      const newItems = reorder(
+        sourceList,
+        result.source.index,
+        result.destination.index
+      );
+      setCardList(source.droppableId, newItems);
+      return;
+    }
+
+    // remove from current location
+    const [removedItem] = sourceList.splice(source.index, 1);
+    setCardList(source.droppableId, sourceList);
+
+    // add to next location
+    destinationList.push(removedItem);
+    console.log(removedItem);
+    setCardList(destination.droppableId, destinationList);
+  };
+  const getCardList = (id) => {
+    switch (id) {
+      case 'myHand':
+        return myHand;
+      case 'op1Hand':
+        return op1Hand;
+      default:
+        return;
+    }
+  };
+  const setCardList = (id, newList) => {
+    switch (id) {
+      case 'myHand':
+        return setMyHand([...newList]);
+      case 'op1Hand':
+        return setOp1Hand([...newList]);
+      default:
+        return;
+    }
+  };
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  return (
     <div className='Hearts'>
-        <TopBar/>
-        
-        <div className='Board'>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Hand
-                    x={100}
-                    y={200}
-                    id="myHand"
-                    cards={myHand}
-                    setCards={setMyHand}
-                />
-                <Hand
-                    x={100}
-                    y={500}
-                    id="op1Hand"
-                    cards={op1Hand}
-                    setCards={setOp1Hand}
-                />
-            </DragDropContext>
-        </div>
-    </div> 
-    );
-}
- 
+      <TopBar />
+
+      <div className='Board'>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Hand
+            x={100}
+            y={200}
+            id='myHand'
+            cards={myHand}
+            setCards={setMyHand}
+          />
+          <Hand
+            x={100}
+            y={500}
+            id='op1Hand'
+            cards={op1Hand}
+            setCards={setOp1Hand}
+          />
+        </DragDropContext>
+      </div>
+    </div>
+  );
+};
+
 export default Hearts;
