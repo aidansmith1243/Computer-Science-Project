34d276c93bea042365c4213861264c5b3b418ec6
Aidan Smith <aidansmith1243@gmail.com>
2021-11-27 17:51:29 -0600

    quick fix to leave a game

--- CardGame/CardGame/client-app/src/Components/TopBar/Topbar.jsx
+++ CardGame/CardGame/client-app/src/Components/TopBar/Topbar.jsx
@@ -1,28 +1,30 @@
 import './TopBar.css';
 import { Navbar, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-const title_style =
-{
-    height: "40px",
-    backgroundColor:"purple",
+const title_style = {
+  height: '40px',
+  backgroundColor: 'purple',
 };
 
 const TopBar = (props) => {
-    const user = props.user;
-    return (
-    <Navbar style={title_style} fixed="top">
-        <Container>
-          <Navbar.Brand>Card Game</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-              {user ?
-            <Navbar.Text className="text-dark">
-              Hello, {user}
-            </Navbar.Text> : <div/>}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-}
- 
+  const user = props.user;
+  const navigate = useNavigate();
+  return (
+    <Navbar style={title_style} fixed='top'>
+      <Container>
+        <Navbar.Brand onClick={() => navigate('/')}>Card Game</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className='justify-content-end'>
+          {user ? (
+            <Navbar.Text className='text-dark'>Hello, {user}</Navbar.Text>
+          ) : (
+            <div />
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
 export default TopBar;
