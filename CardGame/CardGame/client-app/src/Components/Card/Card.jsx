9fa25d1bc681b470a1d994ae9c489cc68792063a
Aidan Smith <aidansmith1243@gmail.com>
2021-12-14 12:32:52 -0600

    Added logo to back of cards

--- CardGame/CardGame/client-app/src/Components/Card/Card.jsx
+++ CardGame/CardGame/client-app/src/Components/Card/Card.jsx
@@ -1,40 +1,71 @@
 import './Card.css';
+import KSULogo from './ksu.png';
 const Card = (props) => {
-    const {suit, rank, position, className} = props;
-    // Suits: H, D, S, C
-    // Ranks: A,2,3,4,5,6,7,8,9,10,J,Q,K
-    const visible = suit !== undefined && rank !== undefined;
-    let symbol = '?';
-    const color = suit === 'H' || suit === 'D' ? 'red' : 'black';
-    switch(suit){
-        case 'H':
-            symbol = '♥';
-            break;
-        case 'D':
-            symbol = '♦';
-            break;
-        case 'S':
-            symbol = '♠';
-            break;
-        case 'C':
-            symbol = '♣';
-            break;
-    }
-    return ( 
+  const { suit, rank, position, className } = props;
+  // Suits: H, D, S, C
+  // Ranks: A,2,3,4,5,6,7,8,9,10,J,Q,K
+  const visible = suit !== undefined && rank !== undefined;
+  let symbol = '?';
+  const color = suit === 'H' || suit === 'D' ? 'red' : 'black';
+  switch (suit) {
+    case 'H':
+      symbol = '♥';
+      break;
+    case 'D':
+      symbol = '♦';
+      break;
+    case 'S':
+      symbol = '♠';
+      break;
+    case 'C':
+      symbol = '♣';
+      break;
+  }
+  return (
     <div className={className !== undefined ? 'Card ' + className : 'Card'}>
-        {visible ? 
+      {visible ? (
         <div>
-            <p className='Rank noselect' style={{color: color, fontWeight: 'bold'}}>{rank}</p>
-            <p className='Suit noselect' style={{color: color}}>{symbol}</p>
-            <p className='LargeSuit noselect' style={{color: color, fontSize: '70px'}}>{symbol}</p>
+          <p
+            className='Rank noselect'
+            style={{ color: color, fontWeight: 'bold' }}
+          >
+            {rank}
+          </p>
+          <p className='Suit noselect' style={{ color: color }}>
+            {symbol}
+          </p>
+          <p
+            className='LargeSuit noselect'
+            style={{ color: color, fontSize: '70px' }}
+          >
+            {symbol}
+          </p>
 
-            <p className='RankUpsideDown noselect' style={{color: color, fontWeight: 'bold'}}>{rank}</p>
-            <p className='SuitUpsideDown noselect' style={{color: color}}>{symbol}</p>
-        </div> : 
+          <p
+            className='RankUpsideDown noselect'
+            style={{ color: color, fontWeight: 'bold' }}
+          >
+            {rank}
+          </p>
+          <p className='SuitUpsideDown noselect' style={{ color: color }}>
+            {symbol}
+          </p>
+        </div>
+      ) : (
         <div>
-            <p></p>
-        </div>}
-    </div>);
-}
- 
+          <img
+            src={KSULogo}
+            style={{
+              position: 'absolute',
+              height: '45px',
+              top: '40px',
+              left: '13px',
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Card;
